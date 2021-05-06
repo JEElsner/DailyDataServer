53ec2e60ce47c440f7584d0f55691331f3169f75
Jonathan Elsner <jelsnerbusiness@outlook.com>
2021-05-05 23:10:28 -0700

    Set up python package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='DailyDataServer',
+      version='0.0.1',
+      description='Web server to store and update time log information',
+      url='https://github.com/JEElsner/DailyDataServer',
+      author='Jonathan Elsner',
+      author_email='jeelsner@outlook.com',
+      packages=find_packages(),
+      python_requires='>=3',
+      # TODO
+      # licence='',
+      # classifiers=[],
+      # keywords='',
+      # project_urls=[]
+      )
