b543c9b823ee22c6a491d0497bdaeaed822c3ede
Jonathan Elsner <jelsnerbusiness@outlook.com>
2021-07-26 12:51:08 -0700

    Modify database
    Add information columns:
    * User creation date
    * Activity description
    * Timestamp of last modification of time log item
    
    Change the following columns:
    * email_confirmed now has a default value of false
    * user_id foreign key for activities now cascade on updates and deletes
    * Recorded server_time for timelog entries defaults to CURRENT_TIMESTAMP
    * user_id foreign key in timelog cascades on delete and update
    * activity_id foreign key in timelog restricts on delete and cascades on
    update

--- DailyDataServer/schema.sql
+++ DailyDataServer/schema.sql
@@ -3,18 +3,20 @@ CREATE TABLE IF NOT EXISTS user (
     username TEXT UNIQUE NOT NULL,
     name TEXT NOT NULL,
     email TEXT NOT NULL,
-    email_confirmed BOOLEAN NOT NULL,
+    email_confirmed BOOLEAN DEFAULT false,
     report_time INTEGER,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 CREATE TABLE IF NOT EXISTS activity (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
+    description TEXT,
     parent_activity INTEGER,
     is_alias BOOLEAN NOT NULL,
     is_placeholder BOOLEAN NOT NULL,
     user_id INTEGER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user (id) FOREIGN KEY (parent_activity) REFERENCES activity (id)
+    FOREIGN KEY (user_id) REFERENCES user (id) FOREIGN KEY (parent_activity) REFERENCES activity (id) ON DELETE CASCASE ON UPDATE CASCADE
 );
 CREATE TABLE IF NOT EXISTS timelog (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -23,11 +25,12 @@ CREATE TABLE IF NOT EXISTS timelog (
     source TEXT NOT NULL,
     latitude FLOAT,
     longitude FLOAT,
-    server_time TIMESTAMP NOT NULL,
+    server_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     record_time TIMESTAMP NOT NULL,
     nominal_time TIMESTAMP NOT NULL,
     nominal_time_timezone_offset FLOAT NOT NULL,
     nominal_time_timezone_name TEXT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user (id),
-    FOREIGN KEY (activity_id) REFERENCES activity (id)
+    last_modification_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (activity_id) REFERENCES activity (id) ON UPDATE CASCADE ON DELETE RESTRICT
 )
